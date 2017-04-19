6b077aa17a8ee2fd93d5a720ff9ee26ff5e85506
orzsikodon <odon.orzsik.17@dartmouth.edu>
2017-04-19 15:06:55 -0400

    react env set up

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
-// change required to es6, import style sheets
-import $ from 'jquery';
-import './style.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// template from: http://stackoverflow.com/questions/31066348/how-can-i-increment-a-number-after-every-1-second-using-javascript
-$(document).ready(() => {
-  let num = 0; // initial 0
+const App = () => {
+  return <div className="test">All the React are belong to us!</div>;
+};
 
-  // increment every second
-  setInterval(() => {
-    $('#main').html(`You have been on this page for: ${num} seconds!`); // Update the value in paragraph
-    num += 1;
-  }, 1000); // Run for each second
-});
+ReactDOM.render(<App />, document.getElementById('main'));
